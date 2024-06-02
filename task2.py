2a7dac27e19af3bc87ecd1fd5485039bf60231d7
blacklightsun <blacklightsun@gmail.com>
2024-06-02 14:26:12 +0300

    task2 is ready

--- task2.py
+++ task2.py
@@ -1,36 +1,52 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.integrate as spi
+
 
 # Визначення функції та межі інтегрування
 def f(x):
     return x ** 2
 
-a = 0  # Нижня межа
-b = 2  # Верхня межа
 
+a = 0  # Нижня межа інтегрування
+b = 2  # Верхня межа інтегрування
 
-experiment_qty = 1000
+# розраховуємо площу фігури теоретично через визначений інтеграл
+result, error = spi.quad(f, a, b)
+
+# кількість точок випробування
+experiment_qty = 10000
+
+# формування випадкових точок випробування
 x_list = list(np.random.uniform(a, b, experiment_qty))
-y_list = list(np.random.uniform(0, 5, experiment_qty))
+y_list = list(np.random.uniform(0, 4, experiment_qty))
 
+# "кидаємо камінці", рахуємо влучання в площу
 i = 0
 for x, y in zip(x_list, y_list):
     # print(x, y)
     if y <= f(x):
         i += 1
 
-print(f'Кількість влучань: {i / experiment_qty}')
+monte_karlo_squear = (i / experiment_qty) * ((f(b) - f(a)) * (b - a))
 
-# plt.scatter(x_list, y_list)
+# виводимо результати розрахунків
+print(f'\nПлоща фігури, розрахована теоретично через визначений інтеграл: {result}')
+print(f'Площа прямокутника, в який вписана фігура = {b - a} x {f(b) - f(a)} = {(f(b) - f(a)) * (b - a)}')
+print(f'Кількість випробувань: {experiment_qty}')
+print(f'Кількість влучань у фігуру: {i}')
+print(f'Частка влучань у фігуру: {i / experiment_qty}')
+print(f'Площа фігури, розрахована практично методом Монте-Карло: {monte_karlo_squear}')
+print(f'Відхилення результату за методом Монте-Карло від результату за методом визначних інтегралів: {round(monte_karlo_squear / result * 100 - 100)}%')
 
-# Створення діапазону значень для x
+# Створення діапазону значень x для графіка
 x = np.linspace(-0.5, 2.5, 300)
 y = f(x)
 
 # Створення графіка
 fig, ax = plt.subplots()
 
-# Малювання функції
+# Малювання функції та точок випробування
 ax.plot(x, y, 'r', linewidth=2)
 ax.scatter(x_list, y_list)
 
